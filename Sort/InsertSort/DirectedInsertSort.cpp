126e069cc8d8b5f99479526e25eb414cd071d345
JingLi <1425168871@qq.com>
2022-06-25 16:32:04 +0800

    直接插入排序

---
+++ Sort/InsertSort/DirectedInsertSort.cpp
@@ -0,0 +1,50 @@
+//
+// @author: Christinlj
+// @date: 2022/6/25 15:56
+// @description: 直接插入排序
+//
+
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef int ElemType;
+
+/**
+ * 直接插入排序,结果为非递减有序
+ * @param a 序列
+ * @param n a中元素数目
+ */
+void DirectedInsertSort(vector<ElemType> &a,int n){
+    int i,j;
+    //依次将a[2]~a[n]插入已排序序列(a[1]...a[i-1])
+    for(i=2;i<=n;i++)
+        if(a[i]<a[i-1]){
+            a[0]=a[i];  //a[0]为哨兵，暂存待插入元素
+            for(j=i-1;a[0]<a[j];j--)//找到应该插入的位置
+                a[j+1]=a[j];
+            //复制到插入位置，退出循环后a[0]>a[j]
+            //所以插入到a[j+1]
+            a[j+1]=a[0];
+        }
+}
+
+int main() {
+    vector<ElemType> a;
+    ElemType x;
+    a.push_back(0);
+    cout<<"Please input sequence:";
+    while(cin>>x){
+        a.push_back(x);
+        if(cin.get()=='\n') break;
+    }
+    cout<<"Before:";
+    for (auto it=a.begin()+1;it!=a.end();it++) {
+        cout<<*it<<' ';
+    }
+    DirectedInsertSort(a,a.size()-1);
+    cout<<"\nAfter:";
+    for (auto it=a.begin()+1;it!=a.end();it++) {
+        cout<<*it<<' ';
+    }
+    return 0;
+}
